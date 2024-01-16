0f6262911190f472ed5774f9782c6b3c9f1fc721
Noman Rafiq <heyimnomee@gmail.com>
2024-01-16 15:16:22 +0500

    pug added

--- 5_flashcards/app.js
+++ 5_flashcards/app.js
@@ -1,7 +1,10 @@
 const express = require('express'); //bring express
 const app = express(); // central part of the server
-
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
 // app.listen(3000); //running the listener alone wont do anything, because it won't send anything back
+app.set('view engine', 'pug'); //set the template engine to pug
+//app.set method sets different settings in express
+//by default express is looking for a folder named 'views' on the root of project
 
 app.get('/', (req, res) => {
     res.send('<h1>Oh Express</h1>');
@@ -9,6 +12,22 @@ app.get('/', (req, res) => {
 app.get('/hello', (req, res) => {
     res.send('<h1>Oh Hello Hello</h1>');
 });
+app.get('/pug', (req, res) => {
+    res.render('index'); //response.render will look inside the views folder and we dont need to put pug extention, only the view name
+});
+app.get('/card', (req, res) => {
+    //we can pass variables(locals) in an object
+    res.render('card', {
+        prompt: 'Who is buried in Grants tomb?',
+        hint: 'Think about whose tomb it is',
+        //   colors,
+    });
+    //     OR
+    //we can use res.locals.prompt to pass in the variables
+    //     res.locals.prompt = 'Who is buried there again?';
+    //     res.render('card');
+});
+app.get('/sandbox', (req, res) => {});
 
 app.listen(4000, () => {
     console.log('The server is running on localhost:4000');
