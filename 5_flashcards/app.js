d17eb52bd6a5b4accb48b0c7b2e9e550e5826364
Noman Rafiq <heyimnomee@gmail.com>
2024-01-16 16:14:21 +0500

    cookie parser and clear cookie

--- 5_flashcards/app.js
+++ 5_flashcards/app.js
@@ -2,41 +2,30 @@ const express = require('express'); //bring express
 const app = express(); // central part of the server
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
-// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
-// app.listen(3000); //running the listener alone wont do anything, because it won't send anything back
 app.set('view engine', 'pug'); //set the template engine to pug
-//app.set method sets different settings in express
-//by default express is looking for a folder named 'views' on the root of project
 
 app.get('/', (req, res) => {
-    res.send('<h1>Oh Express</h1>');
-});
-app.get('/hello', (req, res) => {
-    //     res.send('<h1>Oh Hello Hello</h1>');
-    res.render('hello');
-});
-app.post('/hello', (req, res) => {
-    //     console.log(req.body);
-    //     res.json(req.body);
-    res.render('hello', { name: req.body.username });
+    const name = req.cookies.username;
+    if (name) res.render('index', { name });
+    else res.redirect('/hello');
 });
 
-app.get('/pug', (req, res) => {
-    res.render('index'); //response.render will look inside the views folder and we dont need to put pug extention, only the view name
+app.get('/hello', (req, res) => {
+    res.render('hello');
 });
-app.get('/card', (req, res) => {
-    //we can pass variables(locals) in an object
-    res.render('card', {
-        prompt: 'Who is buried in Grants tomb?',
-        hint: 'Think about whose tomb it is',
-        //   colors,
-    });
-    //     OR
-    //we can use res.locals.prompt to pass in the variables
-    //     res.locals.prompt = 'Who is buried there again?';
-    //     res.render('card');
+
+app.post('/hello', (req, res) => {
+    res.cookie('username', req.body.username);
+    res.redirect('/');
 });
+app.post('/goodbye', (req, res) => {
+    res.clearCookie('username');
+    res.redirect('/hello');
+});
+
 app.get('/sandbox', (req, res) => {});
 
 app.listen(4000, () => {
