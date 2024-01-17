a0b3ec727e503405a7760dd1d2b806d74d1dbf34
Noman Rafiq <heyimnomee@gmail.com>
2024-01-17 19:26:38 +0500

    pug practice

---
+++ 7_pug_practice/routes/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const { recipes } = require('../data/data.json');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    // 1. Pass all recipe data to 'index' template
+    console.log(recipes);
+    res.render('index', { recipes });
+    next(recipes);
+});
+
+/* GET recipe page. */
+router.get('/recipes/:id', function (req, res, next) {
+    const recipeId = req.params.id;
+    const recipe = recipes.find(({ id }) => id === +recipeId);
+
+    if (recipe) {
+        // 2. Pass the recipe data to the 'recipe' template
+        res.render('recipe');
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+module.exports = router;
