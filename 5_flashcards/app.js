2b5229109948a5f87fc64513f1489e97a6dadbfb
Noman Rafiq <heyimnomee@gmail.com>
2024-01-17 11:45:58 +0500

    static and routes added

--- 5_flashcards/app.js
+++ 5_flashcards/app.js
@@ -4,50 +4,14 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
-
+app.use('/static', express.static(__dirname + '/public'));
 app.set('view engine', 'pug'); //set the template engine to pug
 
-//MIDDLEWARES
-app.use((req, res, next) => {
-    req.message = 'This message made it from middleware one!';
-    const err = new Error('Oh noes!');
-    err.status = 500;
-    next();
-});
+const mainRoutes = require('./routes/index.js');
+const cardRoutes = require('./routes/cards.js');
 
-app.use((req, res, next) => {
-    console.log(req.message);
-    next();
-});
-//Error Middleware
-(err, req, res, next) => {};
-
-/////////////////////////////
-app.get('/', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.render('index', { name });
-    } else {
-        res.redirect('/hello');
-    }
-});
-
-app.get('/hello', (req, res) => {
-    if (req.cookies.username) {
-        res.redirect('/');
-    } else {
-        res.render('hello');
-    }
-});
-
-app.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
-});
-app.post('/goodbye', (req, res) => {
-    res.clearCookie('username');
-    res.redirect('/hello');
-});
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
 
 app.use((req, res, next) => {
     const err = new Error('Not Found');
@@ -61,6 +25,7 @@ app.use((err, req, res, next) => {
     res.status(err.status);
     res.render('error');
 });
+
 app.listen(4000, () => {
     console.log('The server is running on localhost:4000');
 });
