7ea500c944879ab850a058e8b5cc8a326de5b1ef
Noman Rafiq <heyimnomee@gmail.com>
2024-01-18 13:54:23 +0500

    methods and codes

--- 10_steve_griffith_lessons/app.js
+++ 10_steve_griffith_lessons/app.js
@@ -1,18 +1,39 @@
 const express = require('express');
 const app = express();
+const http = require('http');
 
 app.get('/', (req, res) => {
-    const response = `
-    <h1>req.url: ${req.url}</h1>`;
+    let response = '<h1>Start from here</h1>';
+    response += `<h2>Header Details: ${req.header}`;
+    response += `<h2>URL Details: ${req.url}`;
+    response += `<h2>IP Details: ${req.ip}`;
+    response += `<h2>Method Details: ${req.method}`;
+    response += `<h2>Protocol Details: ${req.protocol}`;
+    response += `<h2>Path Details: ${req.path}`;
+    response += `<h2>Query Details: ${req.query}`;
+    response += `<h2>Params Details: ${req.params}`;
+    response += `<h2>Subdomains Details: ${req.subdomains}`;
+    response += `<h2>Hostname Details: ${req.hostname}`;
+    response += `<h2>Body Details: ${req.body}`; //to get the body details we might have to parse the data.
+    response += '<h2><a href="/methods">Methods</a></h2>';
+    response += '<h2><a href="/codes">Status Codes</a></h2>';
     return res.send(response);
 });
-
 app.get('/old', (req, res) => {
-    return res.redirect(301, '/new');
+    return res.redirect(301, '/new'); //redirect to new page with status code
+});
+app.get('/new', (req, res) => {
+    return res.send('<h2>Welcome to New page redirected from Old page</h2>');
+});
+//List of Methods
+app.get('/methods', (req, res) => {
+    const methods = 'HTTP Methods: ' + http.METHODS.join(', ');
+    return res.send(methods);
 });
 
-app.get('/new', (req, res) => {
-    return res.send('<h1>Welcome to New Page redirected from Old page');
+//List of status codes
+app.get('/codes', (req, res) => {
+    return res.send(http.STATUS_CODES);
 });
 
 const port = 3000;
@@ -25,4 +46,4 @@ app.listen(port, (err) => {
     }
 });
 
-// Methods: get, post, patch, put, delete
+// Regular Methods: get, post, patch, put, delete, head, options
